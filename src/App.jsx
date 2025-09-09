bda0e0893009ed2a5aee90d85f00e033b30f67aa
john-kelly <johnkellytemena@gmail.com>
2025-09-09 19:37:11 +0800

    Add base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./styles/App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div id="root">
+      <div className="top-banner"></div>
+      <div className="bottom-banner"></div>
+      <main>
+        <div className="left"></div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
