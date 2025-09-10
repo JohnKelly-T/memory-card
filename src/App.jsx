a859449a855f7171c692c8b54c13adea1785b21a
john-kelly <johnkellytemena@gmail.com>
2025-09-10 17:30:48 +0800

    Add function to fetch cards from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import "./styles/App.css";
+import TCGdex from "@tcgdex/sdk";
+import CardContainer from "./components/CardContainer";
+
+async function getCards() {
+  const tcgdex = new TCGdex("en");
+  const sets = await tcgdex.fetch("sets", "sv03.5");
+
+  return sets.cards;
+}
 
 function App() {
+  const [cards, setCards] = useState(null);
+
+  useEffect(() => {
+    getCards().then((result) => setCards(result));
+  }, []);
+
+  let visibleCards = cards ? cards.slice(0, 6) : null;
+
+  console.log(visibleCards);
+
   return (
     <div id="root">
       <div className="top-banner"></div>
       <div className="bottom-banner"></div>
       <main>
         <div className="left"></div>
-        <div className="center"></div>
+        <div className="center">
+          <CardContainer visibleCards={visibleCards} />
+        </div>
         <div className="right"></div>
       </main>
     </div>
