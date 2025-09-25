dee7f7cb362b5dbc5449c9d52fef1b3bb5deae89
john-kelly <johnkellytemena@gmail.com>
2025-09-25 15:29:30 +0800

    Add tilt component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,24 @@
 import pokemonCardBack from "../assets/img/pokemon-card-back.webp";
+import Tilt from "react-parallax-tilt";
 
 function Card({ handleClick, isFlipped, imgSrc }) {
   return (
     <div className={isFlipped ? "card flipped" : "card"} onClick={handleClick}>
-      <div className="card-front">
-        <img src={imgSrc} />
-      </div>
-      <div className="card-back">
-        <img src={pokemonCardBack} />
-      </div>
+      <Tilt
+        className="tilt"
+        glareEnable={true}
+        glareMaxOpacity={0.9}
+        glareColor="#e6ddad"
+        glarePosition="bottom"
+        glareBorderRadius="20px"
+      >
+        <div className="card-front">
+          <img src={imgSrc} />
+        </div>
+        <div className="card-back">
+          <img src={pokemonCardBack} />
+        </div>
+      </Tilt>
     </div>
   );
 }
