575150b4530a534d59b99d03f2aabf51117a3ae6
john-kelly <johnkellytemena@gmail.com>
2025-09-27 15:34:32 +0800

    Add back button onClick callback

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import CardContainer from "../components/CardContainer";
 
-function GamePage({ cards }) {
+function GamePage({ cards, onBackClick }) {
   const [unclickedCards, setUnclickedCards] = useState([]);
   const [clickedCards, setClickedCards] = useState([]);
   const [currentScore, setCurrentScore] = useState(0);
@@ -130,7 +130,24 @@ function GamePage({ cards }) {
 
   return (
     <main className="gamepage">
-      <div className="left"></div>
+      <div className="left">
+        <svg
+          className="back-button"
+          onClick={onBackClick}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 48 48"
+        >
+          <path
+            fill="currentColor"
+            fillRule="evenodd"
+            stroke="currentColor"
+            strokeLinejoin="round"
+            strokeWidth={4}
+            d="M44 40.836q-7.34-8.96-13.036-10.168t-10.846-.365V41L4 23.545L20.118 7v10.167q9.523.075 16.192 6.833q6.668 6.758 7.69 16.836Z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </div>
       <div className="center">
         <CardContainer visibleCards={visibleCards} onClick={handleOnClick} />
       </div>
